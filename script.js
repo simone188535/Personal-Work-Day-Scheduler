fb352568d24240e4a30f7c3773a25cdc2fb2e523
Simone Anthony <simone.anthony1@yahoo.com>
2021-11-30 10:18:31 -0500

    unneeded variables and values removed, code simplified

--- script.js
+++ script.js
@@ -3,32 +3,34 @@ const containerEl = $(".container");
 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-const workHours = ['9:00 AM' , '10:00 AM', '11:00 AM', '12:00 PM', '13:00 PM', '14:00 PM', '15:00 PM', '16:00 PM', '17:00 PM'];
+const workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-$.each(workHours, function (index, value) {
+$.each(workHours, function (index, workHour) {
+    // https://stackoverflow.com/a/40351347/6195136
   // console.log( index + ": " + value );
   // .format("h:mm A")
-  console.log(moment(value, "h:mm A").hour(), today.hour());
-  const workHour = moment(value, "h:mm A");
-// //   const workHour = moment(value, 'h');
-  const workHourFormatted = workHour.format("h:mm A");
-//   const workHourToISO = workHour.toISOString();
-  const currentHourFormatted = today.format("h:mm A");
+//   console.log(moment(value, "h:mm A").hour(), today.hour());
+// ADD TODAYS DATE IN STRING
+//   const workHour = moment(value, "h").hour();
+  const workHourFormatted = moment(workHour, "h").format("h:mm A");
+//   const workHourFormatted = moment(value, "h").format("h:mm A");
+  const currentHour = today.hour();
+
+  console.log('workHour', workHour, 'currentHour', currentHour);
 
   let descriptionClassName; 
-  if (currentHourFormatted === workHourFormatted) {
+  if (workHour === currentHour) {
     descriptionClassName = 'present';
-  } 
-  else if ((workHourFormatted).isBefore(currentHourFormatted)) {
+  } else if (workHour > currentHour) {
     descriptionClassName = 'future';
-  } else if ((workHourFormatted).isAfter(currentHourFormatted)) {
+  } else if (workHour < currentHour) {
     descriptionClassName = 'past';
   }
 
   containerEl.append(`
-    <div class="row">
-        <div class="col-2 time-block d-flex align-items-center justify-content-center" data-time-block="${workHourFormatted}">${workHourFormatted}</div>
-        <div class="col-8 description ${descriptionClassName}"></div>
-        <div class="col-2 saveBtn d-flex align-items-center justify-content-center"><i class="fas fa-save"></i></div>
+    <div class="row time-block">
+        <div class="col-1 hour d-flex align-items-center justify-content-center" data-time-block="${workHour}">${workHourFormatted}</div>
+        <textarea class="col-10 description ${descriptionClassName}"></textarea>
+        <div class="col-1 btn saveBtn d-flex align-items-center justify-content-center"><i class="fas fa-save"></i></div>
       </div>`);
 });
